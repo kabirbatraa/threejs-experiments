7d0072c18c4771becfd6bedecc6cad4ff6e47c26
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-06-23 17:41:27 -0700

    setting position and rotation

--- tutorials/05-transform-objects/src/script.js
+++ tutorials/05-transform-objects/src/script.js
@@ -9,12 +9,29 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
+
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 mesh.position.x = 2
 mesh.position.y = 1
+mesh.position.set(1, 2, -5)
+
+mesh.scale.x = 1.5
+mesh.scale.y = 0.5
+
+mesh.rotation.reorder('YXZ') // make it like fps 
+mesh.rotation.x = Math.PI / 4
+mesh.rotation.y = Math.PI / 8
 scene.add(mesh)
+
+
+
+// draw axes
+const axesHelper = new THREE.AxesHelper(length=0.5);
+scene.add(axesHelper);
+
+
 
 /**
  * Sizes
@@ -28,7 +45,11 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+camera.position.x = 0.5
+camera.position.y = 0.5
+camera.position.z = 2
+
+camera.lookAt(mesh.position)
 scene.add(camera)
 
 /**
@@ -39,3 +60,6 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+console.log(mesh.position.distanceTo(camera.position))
+console.log(mesh.position.length())
