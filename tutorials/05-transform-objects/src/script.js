d41cac9fb5f750158cb111df3b84c48281aed128
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-06-23 17:46:34 -0700

    transformations on groups

--- tutorials/05-transform-objects/src/script.js
+++ tutorials/05-transform-objects/src/script.js
@@ -10,6 +10,32 @@ const scene = new THREE.Scene()
  * Objects
  */
 
+const group = new THREE.Group();
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'green' })
+)
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'yellow' })
+)
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'white' })
+)
+cube2.position.x = -2
+cube3.position.x = 2
+group.add(cube1)
+group.add(cube2)
+group.add(cube3)
+group.rotation.reorder('YXZ')
+group.rotation.y = Math.PI / 8
+group.rotation.x = Math.PI / 4
+
+
+/*
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
@@ -24,11 +50,11 @@ mesh.rotation.reorder('YXZ') // make it like fps
 mesh.rotation.x = Math.PI / 4
 mesh.rotation.y = Math.PI / 8
 scene.add(mesh)
-
+*/
 
 
 // draw axes
-const axesHelper = new THREE.AxesHelper(length=0.5);
+const axesHelper = new THREE.AxesHelper(length=3);
 scene.add(axesHelper);
 
 
@@ -45,11 +71,11 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 0.5
-camera.position.y = 0.5
-camera.position.z = 2
+// camera.position.x = 0.5
+// camera.position.y = 0.5
+camera.position.z = 10
 
-camera.lookAt(mesh.position)
+// camera.lookAt(mesh.position)
 scene.add(camera)
 
 /**
