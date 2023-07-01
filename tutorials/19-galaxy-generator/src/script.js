35225a9ff685f97068f50e507ceff797f672e0f6
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-06-30 18:13:26 -0700

    galaxy variance and randomness

--- tutorials/19-galaxy-generator/src/script.js
+++ tutorials/19-galaxy-generator/src/script.js
@@ -22,11 +22,16 @@ const scene = new THREE.Scene()
  * Galaxy
  */
 const parameters = {
-    count: 1000,
+    count: 10000,
     size: 0.01,
-    radius: 5,
+    radius: 6,
     branches: 3,
-    spin: 1,
+    spin: 6,
+    variance: 1,
+    randomBias: 4,
+    // insideColor: new THREE.Color('#f88437'),
+    // outsideColor: new THREE.Color('#6c2dda')
+
 }
 
 let particleGeometry = null;
@@ -59,10 +64,30 @@ function generateGalaxy() {
         // const spinAngle = parameters.spin * radius
         // I think my spin looks better than his so I'm going to keep mine
 
+        // random for x y z, but decrease randomness as radius increases
+        let varianceValues = []
+        for (let i = 0; i < 3; i++) {
+            varianceValues.push(
+                // Math.pow((Math.random()*2-1) * parameters.variance / radius, parameters.randomBias) 
+                // Math.pow((Math.random()*2-1) * parameters.variance, parameters.randomBias) 
+                // Math.pow(Math.random() * parameters.variance, parameters.randomBias) * (Math.random() < 0.5 ? -1 : 1)
+                Math.pow(Math.random()*parameters.variance, parameters.randomBias) * (Math.random() < 0.5 ? -1 : 1)
+            )
+            // then add additional randomness?
+            // varianceValues[i] += (Math.random()*2-1) * parameters.variance / radius
+
+        }
+        // const variance = new THREE.Vector3(
+        //     (Math.random()*2-1) * parameters.variance / radius,
+        //     (Math.random()*2-1) * parameters.variance / radius,
+        //     (Math.random()*2-1) * parameters.variance / radius
+        // )
+        const variance = new THREE.Vector3(...varianceValues)
+
         // galaxy shape
-        vertices[i*3+0] = radius * Math.sin(angle+spinAngle)
-        vertices[i*3+1] = 0
-        vertices[i*3+2] = radius * Math.cos(angle+spinAngle)
+        vertices[i*3+0] = radius * Math.sin(angle+spinAngle) + variance.x
+        vertices[i*3+1] = 0 + variance.y
+        vertices[i*3+2] = radius * Math.cos(angle+spinAngle) + variance.z
     }
     particleGeometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3))
     // Make sure to use PointsMaterial instead of any other material
@@ -71,7 +96,8 @@ function generateGalaxy() {
         sizeAttenuation: true,
         depthWrite: false,
         blending: THREE.AdditiveBlending,
-        color: 'blue'
+        color: '#f3f49c'
+        // vertexColors: true
     })
     particles = new THREE.Points(particleGeometry, particleMaterial)
     scene.add(particles)
@@ -81,30 +107,46 @@ generateGalaxy()
 
 // can use onFinishChange instead of onChange if framerate too lows
 gui.add(parameters, 'count')
-    .min(100)
-    .max(10000)
-    .step(100)
-    .onChange(generateGalaxy)
+.min(100)
+.max(100000)
+.step(100)
+.onChange(generateGalaxy)
+
 gui.add(parameters, 'size')
-    .min(0.001)
-    .max(0.1)
-    .step(0.001)
-    .onChange(generateGalaxy)
+.min(0.001)
+.max(0.1)
+.step(0.001)
+.onChange(generateGalaxy)
+
 gui.add(parameters, 'radius')
-    .min(0.01)
-    .max(29)
-    .step(0.01)
-    .onChange(generateGalaxy)
+.min(0.01)
+.max(29)
+.step(0.01)
+.onChange(generateGalaxy)
+
 gui.add(parameters, 'branches')
-    .min(2)
-    .max(20)
-    .step(1)
-    .onChange(generateGalaxy)
+.min(2)
+.max(20)
+.step(1)
+.onChange(generateGalaxy)
+
 gui.add(parameters, 'spin')
-    .min(-5)
-    .max(5)
-    .step(1)
-    .onChange(generateGalaxy)
+.min(-5)
+.max(20)
+.step(1)
+.onChange(generateGalaxy)
+
+gui.add(parameters, 'variance')
+.min(0)
+.max(2)
+.step(0.05)
+.onChange(generateGalaxy)
+
+gui.add(parameters, 'randomBias')
+.min(1)
+.max(10)
+.step(0.1)
+.onChange(generateGalaxy)
 
 // problem: we generate a new galaxy but we do not remove the old one from the scene
 
