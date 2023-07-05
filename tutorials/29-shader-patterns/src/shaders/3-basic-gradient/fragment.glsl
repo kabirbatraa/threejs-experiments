207d1578acc6ef609dd2e2609dee9e565193988e
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-07-04 21:43:16 -0700

    rename patterns, pattern 3 and 4 using uv.x and y

--- tutorials/29-shader-patterns/src/shaders/3-basic-gradient/fragment.glsl
+++ tutorials/29-shader-patterns/src/shaders/3-basic-gradient/fragment.glsl
@@ -4,7 +4,7 @@ void main()
 {
     // gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
     vec2 uv = v_uv;
-    float strength = uv.y;
+    float strength = uv.x;
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
