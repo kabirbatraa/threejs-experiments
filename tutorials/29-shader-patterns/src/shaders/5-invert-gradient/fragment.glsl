c6c63330d9e8e6e9e3ee0969745c895b309fb396
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-07-04 21:45:30 -0700

    inverted and squeezed gradients

--- tutorials/29-shader-patterns/src/shaders/5-invert-gradient/fragment.glsl
+++ tutorials/29-shader-patterns/src/shaders/5-invert-gradient/fragment.glsl
@@ -4,7 +4,7 @@ void main()
 {
     // gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
     vec2 uv = v_uv;
-    float strength = uv.y * 10.;
+    float strength = 1. - uv.y;
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
