59751f8187170ec2214e5ec92e93dc75f9240838
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-07-06 21:51:18 -0700

    remove extra import

--- tutorials/43-first-r3f-application/src/index.jsx
+++ tutorials/43-first-r3f-application/src/index.jsx
@@ -1,7 +1,6 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
-import { MeshNormalMaterial } from 'three'
 import Experience from './Experience.jsx'
 import * as THREE from 'three'
 
