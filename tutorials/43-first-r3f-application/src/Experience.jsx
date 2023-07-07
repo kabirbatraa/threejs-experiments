80fdc5f9d1c32a1ec25fb63cfc85d8e7a2e37766
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-07-06 17:39:57 -0700

    animating using useFrame and deltaTime

--- tutorials/43-first-r3f-application/src/Experience.jsx
+++ tutorials/43-first-r3f-application/src/Experience.jsx
@@ -1,17 +1,33 @@
+import { useFrame } from "@react-three/fiber"
+import { useRef } from "react"
+
 export default function Experience() {
+
+    const cube = useRef();
+    const group = useRef();
+
+    useFrame((state, deltaTime) => {
+        console.log('tick') // every frame
+        // to access threejs cube, we need to use cube.current
+        cube.current.rotation.y += deltaTime*10;
+        group.current.rotation.y += deltaTime;
+    })
+
     return <>
-        {/* <mesh>
-            <torusKnotGeometry />
-            <meshNormalMaterial />
-        </mesh> */}
-        <mesh position-x={-2} scale={ 1.5 }>
-            <sphereGeometry args={[1.5, 32, 32]} />
-            <meshBasicMaterial color="mediumpurple" wireframe={true} />
-        </mesh>
-        <mesh position-x={2} rotation-y={Math.PI/4}>
-            <boxGeometry />
-            <meshBasicMaterial color="orange" />
-        </mesh>
+        <group ref={group}>
+            <mesh position-y={3}>
+                <torusKnotGeometry />
+                <meshNormalMaterial />
+            </mesh>
+            <mesh position-x={-2} scale={ 1.5 }>
+                <sphereGeometry args={[1.5, 32, 32]} />
+                <meshBasicMaterial color="mediumpurple" wireframe={true} />
+            </mesh>
+            <mesh ref={cube} position-x={2} rotation-y={Math.PI/4} scale={[3, 0.5, 0.5]}>
+                <boxGeometry />
+                <meshBasicMaterial color="orange" />
+            </mesh>
+        </group>
         <mesh position-y={-2} rotation-x={-Math.PI/2} scale={[10, 2, 0]}>
             <planeGeometry />
             <meshBasicMaterial color="greenyellow" />
