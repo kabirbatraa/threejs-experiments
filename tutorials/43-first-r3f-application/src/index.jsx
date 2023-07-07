6ce6f65c9dc2424f82965f762c04cba70a879ae4
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-07-06 17:20:44 -0700

    basic scene with react three fiber

--- tutorials/43-first-r3f-application/src/index.jsx
+++ tutorials/43-first-r3f-application/src/index.jsx
@@ -1,10 +1,13 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import { MeshNormalMaterial } from 'three'
+import Experience from './Experience.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-    <>
-        Soon to be a badass R3F application
-    </>
+    <Canvas>
+        <Experience />
+    </Canvas>
 )
