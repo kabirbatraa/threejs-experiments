a534fac5696d1a05ef3eaa5732831526a1047317
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-07-06 21:26:20 -0700

    create geometry only once, compute vertex normals after first render

--- tutorials/43-first-r3f-application/src/CustomObject.jsx
+++ tutorials/43-first-r3f-application/src/CustomObject.jsx
@@ -1,22 +1,35 @@
 import * as THREE from 'three';
+import { useRef, useMemo, useEffect } from 'react';
 
 export default function CustomObject() {
 
-    const verticesCount = 10*3;
-    const positions = new Float32Array(verticesCount*3);
+    const geometryRef = useRef();
+    // we cannot set the normals immediately, because the geometry is not yet created
+    useEffect(() => {
+        geometryRef.current.computeVertexNormals()
+    }, []) // run once after the first render
 
-    for (let i = 0; i < verticesCount * 3; i++) {
-        positions[i] = (Math.random() - 0.5) * 4;
-    }
+    const verticesCount = 10*3;
+
+    const positions = useMemo(() => {
+        const positions = new Float32Array(verticesCount*3);
+
+        for (let i = 0; i < verticesCount * 3; i++) {
+            positions[i] = (Math.random() - 0.5) * 4;
+        }
+        return positions;
+    }, [])
+
+    
 
     return <mesh>
-        <bufferGeometry>
+        <bufferGeometry ref={geometryRef}>
             <bufferAttribute 
                 attach="attributes-position"
                 count={verticesCount} 
                 array={positions} 
                 itemSize={3} />
         </bufferGeometry>
-        <meshBasicMaterial color="hotpink" side={THREE.DoubleSide}/>
+        <meshStandardMaterial color="hotpink" side={THREE.DoubleSide}/>
     </mesh>
 }
