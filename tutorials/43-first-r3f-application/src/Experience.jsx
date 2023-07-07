8a9b1ad91cd2c9ea2de1a5742540f84f9e72dab3
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-07-06 17:31:44 -0700

    add some geometry with args

--- tutorials/43-first-r3f-application/src/Experience.jsx
+++ tutorials/43-first-r3f-application/src/Experience.jsx
@@ -1,8 +1,20 @@
 export default function Experience() {
     return <>
-        <mesh>
+        {/* <mesh>
             <torusKnotGeometry />
             <meshNormalMaterial />
+        </mesh> */}
+        <mesh position-x={-2} scale={ 1.5 }>
+            <sphereGeometry args={[1.5, 32, 32]} />
+            <meshBasicMaterial color="mediumpurple" wireframe={true} />
+        </mesh>
+        <mesh position-x={2} rotation-y={Math.PI/4}>
+            <boxGeometry />
+            <meshBasicMaterial color="orange" />
+        </mesh>
+        <mesh position-y={-2} rotation-x={-Math.PI/2} scale={[10, 2, 0]}>
+            <planeGeometry />
+            <meshBasicMaterial color="greenyellow" />
         </mesh>
     </>
 }
