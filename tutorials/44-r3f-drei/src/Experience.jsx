4feef8bc2d9e18ecaa65bf6b8c03bf94cf854d8a
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-07-07 17:54:38 -0700

    add pivot controls

--- tutorials/44-r3f-drei/src/Experience.jsx
+++ tutorials/44-r3f-drei/src/Experience.jsx
@@ -1,5 +1,4 @@
-import { useThree, extend } from '@react-three/fiber'
-import { TransformControls, OrbitControls } from '@react-three/drei'
+import { TransformControls, OrbitControls, PivotControls } from '@react-three/drei'
 
 import { useRef } from 'react'
 
@@ -15,10 +14,19 @@ export default function Experience() {
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+        <PivotControls 
+            anchor={[0,1,0]} // relative positon (without respect to scale)
+            depthTest={false} // render on top of sphere
+            lineWidth={2}
+            axisColors={[ '#9381ff', '#ff4d6d', "#7ae5ff"]}
+            scale={100}
+            fixed={true} // make the size the same, but based on 'scale' pixels
+        >
+            <mesh position-x={ - 2 } scale={[1, 1.5,1]}>
+                <sphereGeometry />
+                <meshStandardMaterial color="orange" />
+            </mesh>
+        </PivotControls>
 
         <mesh ref={cube} position-x={ 2 } scale={ 1.5 } >
             <boxGeometry />
