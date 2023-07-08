214146dc44e3f1ea1abb23ec6838a607e1f444ec
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-07-07 17:27:05 -0700

    use drei to add orbit controls back

--- tutorials/44-r3f-drei/src/Experience.jsx
+++ tutorials/44-r3f-drei/src/Experience.jsx
@@ -1,8 +1,11 @@
 import { useThree, extend } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 export default function Experience()
 {
     return <>
+
+        <OrbitControls />
 
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
