528c27aad9e11f47fa925bdc109a387cd7c094c5
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-07-10 10:46:01 -0700

    add reflector material to plane

--- tutorials/44-r3f-drei/src/Experience.jsx
+++ tutorials/44-r3f-drei/src/Experience.jsx
@@ -1,4 +1,4 @@
-import { Float, Text, TransformControls, OrbitControls, PivotControls, Html } from '@react-three/drei'
+import { MeshReflectorMaterial, Float, Text, TransformControls, OrbitControls, PivotControls, Html } from '@react-three/drei'
 
 import { useRef } from 'react'
 
@@ -47,7 +47,16 @@ export default function Experience() {
 
         <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
+            {/* <meshStandardMaterial color="greenyellow" /> */}
+
+            {/* works only on planar meshes: */}
+            <MeshReflectorMaterial 
+                resolution={1024}
+                blur={[1000, 1000]}
+                mixBlur={0.5}
+                mirror={0.7}
+                color='greenyellow'
+            />
         </mesh>
 
 
