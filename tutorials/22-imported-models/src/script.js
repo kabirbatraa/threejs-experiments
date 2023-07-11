23329a1c55cb0d0e2c54f4a41dc35bff96305b98
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-07-11 16:39:53 -0700

    import gltf model of duck using GLTFLoader

--- tutorials/22-imported-models/src/script.js
+++ tutorials/22-imported-models/src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import * as dat from 'lil-gui'
 
 THREE.ColorManagement.enabled = false
@@ -15,6 +16,25 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/** 
+ * Models
+ */
+const gltfLoader = new GLTFLoader();
+gltfLoader.load(
+    '/models/Duck/glTF/Duck.gltf', // path
+    (gltf) => {
+        console.log("success")
+        console.log(gltf)
+        scene.add(gltf.scene.children[0])
+    },
+    // () => {
+    //     console.log("progress")
+    // },
+    // () => {
+    //     console.log("error")
+    // },
+)
 
 /**
  * Floor
