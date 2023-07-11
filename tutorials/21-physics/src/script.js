8e57277b047ee20edf6bed63c5d9587e75198fb4
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-07-10 17:19:26 -0700

    allow sleeping objects for further optimization

--- tutorials/21-physics/src/script.js
+++ tutorials/21-physics/src/script.js
@@ -41,6 +41,7 @@ const environmentMapTexture = cubeTextureLoader.load([
  */
 const world = new CANNON.World()
 world.broadphase = new CANNON.SAPBroadphase(world)
+world.allowSleep = true;
 world.gravity.set(0, -9.82, 0)
 
 // Materials (physics)
