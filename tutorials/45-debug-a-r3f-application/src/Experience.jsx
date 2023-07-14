054ae88b74157b9b21737786dc84c59b2cca163c
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-07-13 17:11:56 -0700

    invert y direction of joystick

--- tutorials/45-debug-a-r3f-application/src/Experience.jsx
+++ tutorials/45-debug-a-r3f-application/src/Experience.jsx
@@ -7,10 +7,11 @@ export default function Experience()
 
     const { position } = useControls({
         position: {
-            value: {x: -2, y: 0, z: 0 }, 
+            value: {x: -2, y: 0, }, 
             // min: -4,
             // max: 4,
-            step: 0.01
+            step: 0.01,
+            joystick: 'invertY'
         }
     })
 
@@ -21,7 +22,7 @@ export default function Experience()
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
-        <mesh position={ [ position.x, position.y, position.z ] }>
+        <mesh position={ [ position.x, position.y, 0 ] }>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
         </mesh>
