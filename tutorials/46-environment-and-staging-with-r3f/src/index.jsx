ec61b62b8f9e7cf0067c0bd2ffa94057cb35af72
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-07-13 18:45:52 -0700

    set background color using onCreate function callback

--- tutorials/46-environment-and-staging-with-r3f/src/index.jsx
+++ tutorials/46-environment-and-staging-with-r3f/src/index.jsx
@@ -1,9 +1,18 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
+import * as THREE from 'three'
 import Experience from './Experience.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+// function created({gl}) {
+//     gl.setClearColor('#ff00ff', 1)
+// }
+
+function created({scene}) {
+    scene.background = new THREE.Color('cyan')
+}
 
 root.render(
     <Canvas
@@ -13,6 +22,7 @@ root.render(
             far: 200,
             position: [ - 4, 3, 6 ]
         } }
+        onCreated={created}
     >
         <Experience />
     </Canvas>
