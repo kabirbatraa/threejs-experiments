b4d3cfd2fab8c9c0bfff01f0cc01214b4e98d8a4
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-07-13 20:07:37 -0700

    soft shadows using drei helper

--- tutorials/46-environment-and-staging-with-r3f/src/Experience.jsx
+++ tutorials/46-environment-and-staging-with-r3f/src/Experience.jsx
@@ -2,11 +2,13 @@ import { useFrame } from '@react-three/fiber'
 import { 
     useHelper, 
     OrbitControls,
-    BakeShadows
+    BakeShadows,
+    SoftShadows
 } from '@react-three/drei'
 import { useRef } from 'react'
 import { Perf } from 'r3f-perf'
 import * as THREE from 'three'
+import { useControls } from 'leva'
 
 export default function Experience()
 {
@@ -20,9 +22,27 @@ export default function Experience()
         cube.current.rotation.y += delta * 0.2
     })
 
+    const {samples} = useControls({
+        samples: {
+            value: 17,
+            step: 1,
+            min: 1,
+            max: 30
+        }
+    })
+
     return <>
 
-        <BakeShadows />
+        {/* <BakeShadows /> */}
+
+        {/* softshadows modify the shaders in threejs, so modifying these values during runtime will cause lag */}
+        <SoftShadows
+            frustum={3.75}
+            size={50}
+            near={9.5}
+            samples={samples}
+            rings={11}
+        />
 
         <Perf position="top-left" />
 
@@ -34,12 +54,12 @@ export default function Experience()
             intensity={ 1.5 } 
             castShadow={true}
             shadow-mapSize={[1024, 1024]}
-            shadow-camera-left={-2}
-            shadow-camera-right={2}
+            shadow-camera-left={-5}
+            shadow-camera-right={5}
             shadow-camera-bottom={-5}
             shadow-camera-top={5}
             shadow-camera-near={0.1}
-            shadow-camera-far={5}
+            shadow-camera-far={10}
         />
         <ambientLight intensity={ 0.5 } />
 
