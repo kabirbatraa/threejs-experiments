b9f751edba7509ec8d8acfd5b4f75b1c8043047a
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-07-24 22:01:32 -0700

    load model using useLoader(GLTFLoader)

--- tutorials/47-load-models-with-r3f/src/Experience.jsx
+++ tutorials/47-load-models-with-r3f/src/Experience.jsx
@@ -1,8 +1,13 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 export default function Experience()
 {
+
+    const model = useLoader(GLTFLoader, './hamburger.glb');
+
     return <>
 
         <Perf position="top-left" />
@@ -12,7 +17,7 @@ export default function Experience()
         <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
-        <mesh castShadow position-x={ - 2 }>
+        {/* <mesh castShadow position-x={ - 2 }>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
         </mesh>
@@ -20,12 +25,14 @@ export default function Experience()
         <mesh castShadow position-x={ 2 } scale={ 1.5 }>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+        </mesh> */}
 
         <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
+
+        <primitive object={model.scene} scale={0.35} />
 
     </>
 }
