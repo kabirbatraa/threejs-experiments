e17af5aaf4f3edb726f28148640e94079ad80b54
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-08-02 23:30:40 -0700

    make generatePointsInSphere a static function

--- FBOParticles/src/SimulationMaterial.jsx
+++ FBOParticles/src/SimulationMaterial.jsx
@@ -3,27 +3,6 @@ import simulationFragmentShader from './simulationShaders/fragmentShader.js'
 
 import * as THREE from 'three'
 
-function generatePointsInSphere(count) {
-    const positions = new Float32Array(count * 4);
-        
-    for (let i = 0; i < count; i++) {
-
-        const radius = Math.sqrt(Math.random()) * 0.5 + 1.5;
-
-        let x = Math.random() * 2 - 1; // Random value between -1 and 1
-        let y = Math.random() * 2 - 1; // Random value between -1 and 1
-        let z = Math.random() * 2 - 1; // Random value between -1 and 1
-
-        const length = Math.sqrt(x * x + y * y + z * z);
-        x = x / length * radius;
-        y = y / length * radius;
-        z = z / length * radius;
-
-        // we wont use the forth value (rgba texture)
-        positions.set([x, y, z, 1.0], i * 4);
-    }
-}
-
 // custom material that stores the positions of particles in a texture 
 // the texture is passed as a uniform to the fragment shader
 // where uv coordinates are converted to texture coordinates 
@@ -31,7 +10,7 @@ function generatePointsInSphere(count) {
 class SimulationMaterial extends THREE.ShaderMaterial {
     constructor(size) {
 
-        const data = generatePointsInSphere(size * size);
+        const data = SimulationMaterial.generatePointsInSphere(size * size);
         
         const positionsTexture = new THREE.DataTexture(
             data,
@@ -56,6 +35,27 @@ class SimulationMaterial extends THREE.ShaderMaterial {
             vertexShader: simulationVertexShader,
             fragmentShader: simulationFragmentShader,
         })
+    }
+
+    static generatePointsInSphere(count) {
+        const positions = new Float32Array(count * 4);
+            
+        for (let i = 0; i < count; i++) {
+    
+            const radius = Math.sqrt(Math.random()) * 0.5 + 1.5;
+    
+            let x = Math.random() * 2 - 1; // Random value between -1 and 1
+            let y = Math.random() * 2 - 1; // Random value between -1 and 1
+            let z = Math.random() * 2 - 1; // Random value between -1 and 1
+    
+            const length = Math.sqrt(x * x + y * y + z * z);
+            x = x / length * radius;
+            y = y / length * radius;
+            z = z / length * radius;
+    
+            // we wont use the forth value (rgba texture)
+            positions.set([x, y, z, 1.0], i * 4);
+        }
     }
 }
 
