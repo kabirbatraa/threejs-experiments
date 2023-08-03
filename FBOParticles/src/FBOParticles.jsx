5283f5da138e8c37698fca1af16ab26797920eda
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-08-02 23:41:26 -0700

    begin making FBOParticles component

--- FBOParticles/src/FBOParticles.jsx
+++ FBOParticles/src/FBOParticles.jsx
@@ -1,8 +1,29 @@
+import { createPortal, extend } from "@react-three/fiber";
+import { useRef } from "react";
 
+import * as THREE from 'three';
 
+import SimulationMaterial from "./SimulationMaterial";
+extend({ SimulationMaterial: SimulationMaterial });
 
 export default function FBOParticles() {
+
+    // number particles = textureSize^2
+    const textureSize = 128;
+
+    const points = useRef();
+    const simulationMaterialRef = useRef();
+
+    const scene = new THREE.Scene();
+    // const camera 
+
     return <>
-        
+        {/* render off screen */}
+        {createPortal(
+            <mesh>
+                <simulationMaterial />
+            </mesh>,
+            scene
+        )}
     </>
 }
