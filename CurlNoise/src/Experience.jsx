f147be098c7c3fe8eb07e2dbc6606009d8db003e
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-08-04 16:57:58 -0700

    new CurlNoise project based on particle sphere

---
+++ CurlNoise/src/Experience.jsx
@@ -0,0 +1,25 @@
+import { useFrame } from '@react-three/fiber'
+import { 
+    OrbitControls,
+} from '@react-three/drei'
+import { Perf } from 'r3f-perf'
+import TutorialParticles from './TutorialParticles'
+
+export default function Experience() {
+
+    useFrame((state, delta) => {
+        
+    })
+
+    const count = 9001;
+
+    return <>
+
+        {/* <Perf position="top-left" /> */}
+        <OrbitControls makeDefault />
+        {/* <OrbitControls autoRotate /> */}
+
+        <TutorialParticles count={count} />
+
+    </>
+}
---
+++ CurlNoise/src/tutorialShaders/fragmentShader.glsl
@@ -0,0 +1,27 @@
+uniform vec3 uColorA;
+uniform vec3 uColorB;
+
+varying float vRadius;
+
+void main() {
+
+  // create strength equal to distance from center of particle
+  float strength = 1.0 - distance(gl_PointCoord, vec2(0.5));
+
+  // exaggerate the strength curve
+  strength = pow(strength, 3.0);
+
+  if (strength < 0.2) {
+    strength = 0.0;
+  }
+
+
+  // set base color between color A and B by radius
+  vec3 color = mix(uColorA, uColorB, 1. - vRadius);
+
+  // set end color to between black and base color based on strength
+  color = mix(vec3(0.0), color, strength);
+
+  // also base opacity off of strength
+  gl_FragColor = vec4(color, strength);
+}
